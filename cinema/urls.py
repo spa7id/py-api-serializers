f4e7d9b66ea6509c2d5d4eddfabee7035ca080c5
Volodymyr Pankiv <ivovan4ik@gmail.com>
2024-12-11 20:33:11 +0200

    manage urls file

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,8 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/cinema/", include("cinema.urls", namespace="cinema"))
+]
 # write urls here
